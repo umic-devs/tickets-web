8bc09de6ea0b5145e90832044b14da5f6f492c96
Igor Aragão <roginaldosemog@gmail.com>
2020-10-06 16:32:16 -0300

    Estrutura pastas internas do projeto

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+import App from './components/App';
+import * as serviceWorker from './services/serviceWorker';
 
 import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
