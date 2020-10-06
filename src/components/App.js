a3b5b8ec640f4ecf1135723f18c9560e22bf0c95
Igor Aragão <roginaldosemog@gmail.com>
2020-10-06 17:14:10 -0300

    Adiciona texto e formatação da página inicial

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from '../assets/images/logo.svg';
-import './App.css';
+import React from "react";
+import logo from "../assets/images/logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="min-vh-100 py-5 o-hidden">
+      <div className="container position-relative">
+        <div className="row justify-content-center mb-md-5">
+          <div className="col-auto">
+            <img src={logo} alt="logo" style={{ height: 60, width: "auto" }} />
+          </div>
+        </div>
+        <div className="row justify-content-center py-6">
+          <div className="col text-center">
+            <h1>Novidades em Breve!</h1>
+            <div className="lead">
+              Haaaja coração! Estamos preparando algo muito especial para os
+              jovens da Igreja de Cristo. Fique ligado!
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
