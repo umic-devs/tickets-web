a8d2aef0c844e4c0ef53ca97d03a5ebec4e500d8
Igor Aragão <roginaldosemog@gmail.com>
2020-10-12 22:30:30 -0300

    Adiciona design do contador (sem funcionalidade)

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -17,10 +17,43 @@ export default function ComingSoon() {
       <div className="container layer-2">
         <div className="row min-vh-80 align-items-center">
           <div className="col-12 text-center">
-            <img className="mb-4" src={logo} alt="logo" style={{ height: 76, width: "auto" }} />
+            <img
+              className="mb-4 mt-4 mt-md-0"
+              src={logo}
+              alt="logo"
+              style={{ height: 76, width: "auto" }}
+            />
             <h1>Tem Novidade Vindo Aí!</h1>
             <div className="lead">
               Seguuura, estamos preparando algo pra vocês. Fique ligado!
+            </div>
+            <div className="row mt-4 text-center text-body">
+              <div className="col-md">
+                <div className="card card-body">
+                  <span className="h1 text-primary mb-2">00</span>
+                  <span className="h6 mb-0">Dias</span>
+                </div>
+              </div>
+
+              <div className="col-md">
+                <div className="card card-body">
+                  <span className="h1 text-primary mb-2">01</span>
+                  <span className="h6 mb-0">Horas</span>
+                </div>
+              </div>
+
+              <div className="col-md">
+                <div className="card card-body">
+                  <span className="h1 text-primary mb-2">48</span>
+                  <span className="h6 mb-0">Minutos</span>
+                </div>
+              </div>
+              <div className="col-md">
+                <div className="card card-body">
+                  <span className="h1 text-primary mb-2">44</span>
+                  <span className="h6 mb-0">Segundos</span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
