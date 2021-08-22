48f6b6f394fced2de29806289c78a7dff9a28287
Igor Aragão <roginaldosemog@gmail.com>
2021-08-22 15:52:26 -0300

    Adiciona icones insta e youtube na Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { FaInstagram, FaYoutube } from 'react-icons/fa';
 
 import logo from "../assets/images/umic/icon_logo-white.png";
 
 export default function Navbar({ activeTab }) {
 
+  /*
   function isActive(tab) {
     const active = tab === activeTab ? "active" : "";
     return active;
   }
+  */
 
   return (
     <nav className="navbar navbar-expand-sm">
@@ -17,6 +20,7 @@ export default function Navbar({ activeTab }) {
         </a>
         <div className="justify-content-end d-none d-sm-block">
           <ul className="navbar-nav">
+            {/*
             <li className={`nav-item ${isActive("comic")}`}>
               <a className="nav-link" href="/comic">
                 COMIC
@@ -31,6 +35,27 @@ export default function Navbar({ activeTab }) {
               <a className="nav-link" href="/blog">
                 BLOG
               </a>
+            </li>
+            */}
+            <li className={`nav-item`}>
+              <a
+                className="nav-link"
+                href="https://www.instagram.com/umicbrasil/"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <FaInstagram className="mb-1" size={19}/>
+                </a>
+            </li>
+            <li className={`nav-item`}>
+              <a
+                className="nav-link"
+                href="https://www.youtube.com/c/UMICBrasil/videos"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <FaYoutube className="mb-1" size={21}/>
+                </a>
             </li>
           </ul>
         </div>
