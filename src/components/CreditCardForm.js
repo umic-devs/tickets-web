fd0cef4d3837bf4566e80381e5dcad88417e5f18
Leandro Alves <leandrosustenido@gmail.com>
2022-01-25 21:58:20 -0300

    add credit card form validation

--- src/components/CreditCardForm.js
+++ src/components/CreditCardForm.js
@@ -2,9 +2,21 @@ import React from "react";
 import { FaCreditCard, FaLock } from "react-icons/fa";
 import { Spinner } from '../components'
 import { useForm } from "react-hook-form";
+import {
+  CREDIT_CARD_PATTERN,
+  CVV_PATTERN,
+  FULL_NAME_PATTERN,
+  MONTH_PATTERN,
+  YEAR_PATTERN,
+} from '../constants'
+
 
 export default function CreditCardForm({ formData, onSubmit, isLoading }) {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const totalValue = composeCurrency(formData.total)
 
@@ -23,13 +35,22 @@ export default function CreditCardForm({ formData, onSubmit, isLoading }) {
                 <div className="form-group">
                   <label htmlFor="name">Nome</label>
                   <input
-                    className="form-control"
+                    className={`form-control ${errors.cardHolderName && 'is-invalid'}`}
                     disabled={isLoading}
                     id="name"
                     type="text"
                     placeholder="Nome que está no cartão"
-                    {...register("cardHolderName", { required: true })}
+                    {...register("cardHolderName", {
+                        required: true,
+                        pattern: FULL_NAME_PATTERN
+                    })}
                   />
+
+                  {errors.cardHolderName && (
+                    <div className="invalid-feedback text-left">
+                      Nome inválido
+                    </div>
+                  )}
                 </div>
               </div>
             </div>
@@ -40,11 +61,14 @@ export default function CreditCardForm({ formData, onSubmit, isLoading }) {
                   <label htmlFor="ccnumber">Número do Cartão</label>
                   <div className="input-group">
                     <input
-                      className="form-control"
+                      className={`form-control ${errors.cardNumber && 'is-invalid'}`}
                       disabled={isLoading}
                       type="text"
                       placeholder="0000 0000 0000 0000"
-                      {...register("cardNumber", { required: true })}
+                      {...register("cardNumber", {
+                        required: true,
+                        pattern: CREDIT_CARD_PATTERN
+                      })}
                     />
                     <div className="input-group-append">
                       <span className="input-group-text">
@@ -52,6 +76,12 @@ export default function CreditCardForm({ formData, onSubmit, isLoading }) {
                       </span>
                     </div>
                   </div>
+
+                  {errors.cardNumber && (
+                    <div className="invalid-feedback text-left">
+                      Numero de cartão inválido
+                    </div>
+                  )}
                 </div>
               </div>
             </div>
@@ -65,8 +95,17 @@ export default function CreditCardForm({ formData, onSubmit, isLoading }) {
                   id="ccmonth"
                   type="text"
                   placeholder="MM"
-                  {...register("cardExpirationMonth", { required: true })}
+                  {...register("cardExpirationMonth", {
+                    required: true,
+                    pattern: MONTH_PATTERN
+                  })}
                 />
+
+                {errors.cardExpirationMonth && (
+                  <div className="invalid-feedback text-left">
+                    Mes inválido
+                  </div>
+                )}
               </div>
 
               <div className="form-group col-sm-4">
@@ -77,8 +116,17 @@ export default function CreditCardForm({ formData, onSubmit, isLoading }) {
                   id="ccyear"
                   type="text"
                   placeholder="AAAA"
-                  {...register("cardExpirationYear", { required: true })}
+                  {...register("cardExpirationYear", {
+                    required: true,
+                    pattern: YEAR_PATTERN
+                  })}
                 />
+
+                {errors.cardExpirationYear && (
+                  <div className="invalid-feedback text-left">
+                    Ano inválido
+                  </div>
+                )}
               </div>
 
               <div className="col-sm-4">
@@ -89,9 +137,18 @@ export default function CreditCardForm({ formData, onSubmit, isLoading }) {
                     disabled={isLoading}
                     id="cvv"
                     type="text"
-                    placeholder=""
-                    {...register("cardSecurityCode", { required: true })}
+                    placeholder="000"
+                    {...register("cardSecurityCode", {
+                      required: true,
+                      pattern: CVV_PATTERN
+                     })}
                   />
+
+                  {errors.cardSecurityCode && (
+                    <div className="invalid-feedback text-left">
+                      CVV inválido
+                    </div>
+                  )}
                 </div>
               </div>
             </div>
---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const FULL_NAME_PATTERN = /^[a-zA-Z]{4,}(?: [a-zA-Z]+){0,2}$/
+export const CREDIT_CARD_PATTERN = /^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/
+export const MONTH_PATTERN = /^(0?[1-9]|1[012])$"/
+export const YEAR_PATTERN = /^(20)\d{2}$/
+export const CVV_PATTERN = /^\d{3}$/
